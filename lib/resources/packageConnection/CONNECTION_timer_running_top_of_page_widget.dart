e575edf80a73c9b297ff5038edfc30c0546f2fe4
Tejas Dhanda <tejas11personal@gmail.com>
2021-07-20 12:23:24 +0530

    top timer widget was being disposed after creating a new one(that's insane) but repaired that by checking if dispose is required

--- lib/resources/packageConnection/CONNECTION_timer_running_top_of_page_widget.dart
+++ lib/resources/packageConnection/CONNECTION_timer_running_top_of_page_widget.dart
@@ -1,10 +1,10 @@
+import 'package:custom_highly_reusable_functions/HighlyReusableFunctions.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lecture_progress/mainImplementation/allStates/statesOfAllPages.dart';
 
 import 'package:lecture_progress/mainImplementation/temp_variables/temp_variables_timer.dart';
 import 'package:custom_top_timer_widget/top_timer_widget.dart';
-
 
 class TimerStatusOnTopOfPage extends StatefulWidget {
   const TimerStatusOnTopOfPage({Key? key}) : super(key: key);
@@ -16,21 +16,23 @@ class TimerStatusOnTopOfPage extends StatefulWidget {
 class _TimerStatusOnTopOfPageState extends State<TimerStatusOnTopOfPage> {
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     STATE_TopTimerWidget = setState;
-    
+    // customPrint('created new top timer');
   }
 
   @override
   void dispose() {
-    // TODO: implement dispose
     super.dispose();
-    STATE_TopTimerWidget = null;
+    if (STATE_TopTimerWidget == setState) {
+      STATE_TopTimerWidget = null;
+      // customPrint('disposed new top timer');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    // customPrint('Setstate CONNECTION');
     return CustomTopTimerWidget(
       TV_howLong: TV_howLong,
       TV_timeSpent: TV_timeSpent,
